c1f78e947b444e8e693b447a383607728bd19833
hector <hectornuno1994@gmail.com>
2023-06-13 16:45:40 -0700

    adds basic styling to page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,45 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {logIn, personCircle} from "ionicons/icons";
 import React from 'react';
 
 const Login: React.FC = () => {
 
+const doLogin = (event: any) => {
+  event.preventDefault();
+  console.log("logged in");
+};
+
   return (
     <IonPage>
+
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color={"primary"}>
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        UI goes here...
+
+      <IonContent scrollY={false}>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doLogin}>
+              <IonInput label="Email" type='email' placeholder='email@example.com' labelPlacement="floating" fill='outline'></IonInput>
+
+              <IonInput className="ion-margin-top" label="Password" type="password" placeholder='password' labelPlacement="floating" fill='outline'></IonInput>
+
+              <IonButton type="submit" expand="block" className="ion-margin-top">
+              Login 
+              <IonIcon icon={logIn} slot="end"></IonIcon>
+              </IonButton>
+
+              <IonButton color={'secondary'} expand="block" className="ion-margin-top" routerLink="/register">
+                Create account
+              <IonIcon icon={personCircle} slot="end"></IonIcon>
+                </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
+      
     </IonPage>
   );
 };
