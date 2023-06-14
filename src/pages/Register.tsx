ae5e25a8525801d271c60ec32f7e00a02391fbb2
hector <hectornuno1994@gmail.com>
2023-06-13 18:16:50 -0700

    builds code for register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,18 +1,53 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonBackButton, IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
+import { addCircle } from 'ionicons/icons';
+import React, { useState } from 'react';
+import Gamepad from "../assets/gamepad.svg"
 
 const Register: React.FC = () => {
+  const router = useIonRouter();
+
+  const [introSeen, setIntroSeen] = useState(false);
+
+  const doRegister = (event: any) => {
+    event.preventDefault();
+    console.log("registered");  
+    router.goBack()
+  };
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Register</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        UI goes here...
-      </IonContent>
+
+    <IonHeader>
+      <IonToolbar color={"primary"}>
+
+        <IonButton slot="start">
+          <IonBackButton defaultHref="/"></IonBackButton>
+        </IonButton>
+
+        <IonTitle>Create Account</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent scrollY={false}>
+
+      <img src={Gamepad} alt="gamepad logo" width={"50%"} />
+
+      <IonCard>
+        <IonCardContent>
+          <form onSubmit={doRegister}>
+            <IonInput label="Email" type='email' placeholder='email@example.com' labelPlacement="floating" fill='outline'></IonInput>
+
+            <IonInput className="ion-margin-top" label="Password" type="password" placeholder='password' labelPlacement="floating" fill='outline'></IonInput>
+
+            <IonButton type="submit" color={'secondary'} expand="block" className="ion-margin-top" routerLink="/register">
+              Create account
+            <IonIcon icon={addCircle} slot="end"></IonIcon>
+              </IonButton>
+          </form>
+        </IonCardContent>
+      </IonCard>
+    </IonContent>
+
     </IonPage>
   );
 };
