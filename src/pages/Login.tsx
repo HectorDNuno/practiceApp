f1902d438cf64f1b6a4d838c817e02e0fe77019f
hector <hectornuno1994@gmail.com>
2023-06-16 18:25:21 -0700

    adds code from register, stores introSeen state in preferences, creates button to see intro again

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import { Preferences } from "@capacitor/preferences";
+import React, { useEffect, useState } from "react";
 import {
   IonButton,
   IonCard,
@@ -11,11 +13,24 @@ import {
   IonToolbar,
   useIonRouter,
 } from "@ionic/react";
-import { logIn, personCircle } from "ionicons/icons";
-import React from "react";
+import { logIn, personCircle, videocam } from "ionicons/icons";
+import Intro from "../components/Intro";
+
+const INTRO_KEY = "intro-seen";
 
 const Login: React.FC = () => {
   const router = useIonRouter();
+  const [introSeen, setIntroSeen] = useState(true);
+
+  useEffect(() => {
+    const checkStorage = async () => {
+      const seen = await Preferences.get({ key: INTRO_KEY });
+      console.log("seen value:", seen);
+      setIntroSeen(seen.value === "true");
+    };
+
+    checkStorage();
+  }, []);
 
   const doLogin = (event: any) => {
     event.preventDefault();
@@ -23,49 +38,70 @@ const Login: React.FC = () => {
     // router.push("/home", "root");
   };
 
+  const finishIntro = async () => {
+    setIntroSeen(true);
+    Preferences.set({ key: INTRO_KEY, value: "true" });
+  };
+
+  const seeIntroAgain = () => {
+    setIntroSeen(false);
+    Preferences.remove({ key: INTRO_KEY });
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar color={"primary"}>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    <>
+      {!introSeen ? (
+        <Intro onFinish={finishIntro} />
+      ) : (
+        <IonPage>
+          <IonHeader>
+            <IonToolbar color={"primary"}>
+              <IonTitle>Login</IonTitle>
+            </IonToolbar>
+          </IonHeader>
 
-      <IonContent scrollY={false}>
-        <IonCard>
-          <IonCardContent>
-            <form onSubmit={doLogin}>
-              <IonInput
-                label="Email"
-                type="email"
-                placeholder="email@example.com"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
+          <IonContent scrollY={false}>
+            <IonCard>
+              <IonCardContent>
+                <form onSubmit={doLogin}>
+                  <IonInput
+                    label="Email"
+                    type="email"
+                    placeholder="email@example.com"
+                    labelPlacement="floating"
+                    fill="outline"
+                  ></IonInput>
 
-              <IonInput
-                className="ion-margin-top"
-                label="Password"
-                type="password"
-                placeholder="password"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
+                  <IonInput
+                    className="ion-margin-top"
+                    label="Password"
+                    type="password"
+                    placeholder="password"
+                    labelPlacement="floating"
+                    fill="outline"
+                  ></IonInput>
 
-              <IonButton type="submit" expand="block" className="ion-margin-top">
-                Login
-                <IonIcon icon={logIn} slot="end"></IonIcon>
-              </IonButton>
+                  <IonButton type="submit" expand="block" className="ion-margin-top">
+                    Login
+                    <IonIcon icon={logIn} slot="end"></IonIcon>
+                  </IonButton>
 
-              <IonButton color={"secondary"} expand="block" className="ion-margin-top" routerLink="/register">
-                Create account
-                <IonIcon icon={personCircle} slot="end"></IonIcon>
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
-    </IonPage>
+                  <IonButton color={"secondary"} expand="block" className="ion-margin-top" routerLink="/register">
+                    Create account
+                    <IonIcon icon={personCircle} slot="end"></IonIcon>
+                  </IonButton>
+
+                  <IonButton onClick={seeIntroAgain} expand="block" size="small" fill="clear" color={"medium"}>
+                    Watch intro again
+                    <IonIcon icon={videocam} slot="end"></IonIcon>
+                  </IonButton>
+                </form>
+              </IonCardContent>
+            </IonCard>
+          </IonContent>
+        </IonPage>
+      )}
+    </>
   );
 };
 
