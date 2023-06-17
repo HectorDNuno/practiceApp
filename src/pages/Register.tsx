9b79ac3253cf57987187806a060c186504d484ec
hector <hectornuno1994@gmail.com>
2023-06-16 17:15:12 -0700

    adds container props prop, and creates temp function for props

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -16,6 +16,7 @@ import {
 import { addCircle } from "ionicons/icons";
 import React, { useState } from "react";
 import Profile from "../assets/profile.svg";
+import Intro from "../components/Intro";
 
 const Register: React.FC = () => {
   const router = useIonRouter();
@@ -28,58 +29,63 @@ const Register: React.FC = () => {
     router.goBack();
   };
 
+  const finishIntro = async () => {
+    console.log("ça fini");
+  };
+
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar color={"primary"}>
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/"></IonBackButton>
-          </IonButtons>
-
-          <IonTitle>Create Account</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent scrollY={false}>
-        <div className="ion-text-center ion-padding">
-          <img src={Profile} alt="gamepad logo" width={"20%"} />
-        </div>
-
-        <IonCard>
-          <IonCardContent>
-            <form onSubmit={doRegister}>
-              <IonInput
-                label="Email"
-                type="email"
-                placeholder="email@example.com"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-
-              <IonInput
-                className="ion-margin-top"
-                label="Password"
-                type="password"
-                placeholder="password"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-
-              <IonButton
-                type="submit"
-                color={"secondary"}
-                expand="block"
-                className="ion-margin-top"
-                routerLink="/register"
-              >
-                Create account
-                <IonIcon icon={addCircle} slot="end"></IonIcon>
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
-      </IonContent>
-    </IonPage>
+    <>
+      {!introSeen ? (
+        <Intro onFinish={finishIntro} />
+      ) : (
+        <IonPage>
+          <IonHeader>
+            <IonToolbar color={"primary"}>
+              <IonButtons slot="start">
+                <IonBackButton defaultHref="/"></IonBackButton>
+              </IonButtons>
+              <IonTitle>Create Account</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent scrollY={false}>
+            <div className="ion-text-center ion-padding">
+              <img src={Profile} alt="gamepad logo" width={"20%"} />
+            </div>
+            <IonCard>
+              <IonCardContent>
+                <form onSubmit={doRegister}>
+                  <IonInput
+                    label="Email"
+                    type="email"
+                    placeholder="email@example.com"
+                    labelPlacement="floating"
+                    fill="outline"
+                  ></IonInput>
+                  <IonInput
+                    className="ion-margin-top"
+                    label="Password"
+                    type="password"
+                    placeholder="password"
+                    labelPlacement="floating"
+                    fill="outline"
+                  ></IonInput>
+                  <IonButton
+                    type="submit"
+                    color={"secondary"}
+                    expand="block"
+                    className="ion-margin-top"
+                    routerLink="/register"
+                  >
+                    Create account
+                    <IonIcon icon={addCircle} slot="end"></IonIcon>
+                  </IonButton>
+                </form>
+              </IonCardContent>
+            </IonCard>
+          </IonContent>
+        </IonPage>
+      )}
+    </>
   );
 };
 
