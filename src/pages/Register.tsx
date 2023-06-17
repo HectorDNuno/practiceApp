1f33f66934b674086f316f488ddba46f977b7da4
hector <hectornuno1994@gmail.com>
2023-06-16 17:55:02 -0700

    removes code meant for login page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -14,14 +14,11 @@ import {
   useIonRouter,
 } from "@ionic/react";
 import { addCircle } from "ionicons/icons";
-import React, { useState } from "react";
+import React from "react";
 import Profile from "../assets/profile.svg";
-import Intro from "../components/Intro";
 
 const Register: React.FC = () => {
   const router = useIonRouter();
-
-  const [introSeen, setIntroSeen] = useState(false);
 
   const doRegister = (event: any) => {
     event.preventDefault();
@@ -29,63 +26,53 @@ const Register: React.FC = () => {
     router.goBack();
   };
 
-  const finishIntro = async () => {
-    console.log("ça fini");
-  };
-
   return (
-    <>
-      {!introSeen ? (
-        <Intro onFinish={finishIntro} />
-      ) : (
-        <IonPage>
-          <IonHeader>
-            <IonToolbar color={"primary"}>
-              <IonButtons slot="start">
-                <IonBackButton defaultHref="/"></IonBackButton>
-              </IonButtons>
-              <IonTitle>Create Account</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent scrollY={false}>
-            <div className="ion-text-center ion-padding">
-              <img src={Profile} alt="gamepad logo" width={"20%"} />
-            </div>
-            <IonCard>
-              <IonCardContent>
-                <form onSubmit={doRegister}>
-                  <IonInput
-                    label="Email"
-                    type="email"
-                    placeholder="email@example.com"
-                    labelPlacement="floating"
-                    fill="outline"
-                  ></IonInput>
-                  <IonInput
-                    className="ion-margin-top"
-                    label="Password"
-                    type="password"
-                    placeholder="password"
-                    labelPlacement="floating"
-                    fill="outline"
-                  ></IonInput>
-                  <IonButton
-                    type="submit"
-                    color={"secondary"}
-                    expand="block"
-                    className="ion-margin-top"
-                    routerLink="/register"
-                  >
-                    Create account
-                    <IonIcon icon={addCircle} slot="end"></IonIcon>
-                  </IonButton>
-                </form>
-              </IonCardContent>
-            </IonCard>
-          </IonContent>
-        </IonPage>
-      )}
-    </>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color={"primary"}>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/"></IonBackButton>
+          </IonButtons>
+          <IonTitle>Create Account</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent scrollY={false}>
+        <div className="ion-text-center ion-padding">
+          <img src={Profile} alt="gamepad logo" width={"20%"} />
+        </div>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={doRegister}>
+              <IonInput
+                label="Email"
+                type="email"
+                placeholder="email@example.com"
+                labelPlacement="floating"
+                fill="outline"
+              ></IonInput>
+              <IonInput
+                className="ion-margin-top"
+                label="Password"
+                type="password"
+                placeholder="password"
+                labelPlacement="floating"
+                fill="outline"
+              ></IonInput>
+              <IonButton
+                type="submit"
+                color={"secondary"}
+                expand="block"
+                className="ion-margin-top"
+                routerLink="/register"
+              >
+                Create account
+                <IonIcon icon={addCircle} slot="end"></IonIcon>
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
