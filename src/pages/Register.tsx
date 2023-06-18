bdc18b3260c54e87990d0fb8cc17ed7ed0a8404f
hector <hectornuno1994@gmail.com>
2023-06-17 20:31:43 -0700

    wraps card and image in IonRow and IonCol hooks to make page reponsive on web

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,11 +4,14 @@ import {
   IonButtons,
   IonCard,
   IonCardContent,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonIcon,
   IonInput,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
   useIonRouter,
@@ -36,41 +39,53 @@ const Register: React.FC = () => {
           <IonTitle>Create Account</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent scrollY={false}>
-        <div className="ion-text-center ion-padding">
-          <img src={Profile} alt="gamepad logo" width={"20%"} />
-        </div>
-        <IonCard>
-          <IonCardContent>
-            <form onSubmit={doRegister}>
-              <IonInput
-                label="Email"
-                type="email"
-                placeholder="email@example.com"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-              <IonInput
-                className="ion-margin-top"
-                label="Password"
-                type="password"
-                placeholder="password"
-                labelPlacement="floating"
-                fill="outline"
-              ></IonInput>
-              <IonButton
-                type="submit"
-                color={"secondary"}
-                expand="block"
-                className="ion-margin-top"
-                routerLink="/register"
-              >
-                Create account
-                <IonIcon icon={addCircle} slot="end"></IonIcon>
-              </IonButton>
-            </form>
-          </IonCardContent>
-        </IonCard>
+        <IonGrid fixed>
+          <IonRow class="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <div className="ion-text-center ion-padding">
+                <img src={Profile} alt="gamepad logo" width={"20%"} />
+              </div>
+            </IonCol>
+          </IonRow>
+
+          <IonRow class="ion-justify-content-center">
+            <IonCol size="12" sizeMd="8" sizeLg="6" sizeXl="4">
+              <IonCard>
+                <IonCardContent>
+                  <form onSubmit={doRegister}>
+                    <IonInput
+                      label="Email"
+                      type="email"
+                      placeholder="email@example.com"
+                      labelPlacement="floating"
+                      fill="outline"
+                    ></IonInput>
+                    <IonInput
+                      className="ion-margin-top"
+                      label="Password"
+                      type="password"
+                      placeholder="password"
+                      labelPlacement="floating"
+                      fill="outline"
+                    ></IonInput>
+                    <IonButton
+                      type="submit"
+                      color={"secondary"}
+                      expand="block"
+                      className="ion-margin-top"
+                      routerLink="/register"
+                    >
+                      Create account
+                      <IonIcon icon={addCircle} slot="end"></IonIcon>
+                    </IonButton>
+                  </form>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
