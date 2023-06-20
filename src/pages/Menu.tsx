80e06654dbd2c9bec469a5e78b43ee887b5a2647
hector <hectornuno1994@gmail.com>
2023-06-19 19:23:32 -0700

    creates array for menu items, maps through array and return them into IonItem components, adds routes for each menu item

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,15 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonMenu,
+  IonMenuToggle,
+  IonPage,
+  IonRouterOutlet,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import React from "react";
+import { Redirect, Route } from "react-router";
+import List from "./List";
+import Settings from "./Settings";
+import { cogSharp, homeSharp } from "ionicons/icons";
 
 const Menu: React.FC = () => {
+  const paths = [
+    { name: "Home", url: "/app/list", icon: homeSharp },
+    { name: "Settings", url: "/app/settings", icon: cogSharp },
+  ];
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonMenu contentId="main">
+        <IonHeader>
+          <IonToolbar color={"secondary"}>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent className="ion-padding">
+          {paths.map((item, index) => {
+            return (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="none">
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            );
+          })}
+        </IonContent>
+      </IonMenu>
+
+      <IonRouterOutlet id="main">
+        <Route exact path="/app/list" component={List} />
+        <Route exact path="/app/settings" component={Settings} />
+        <Route exact path="/app" component={List}>
+          <Redirect to="/app/list" />
+        </Route>
+      </IonRouterOutlet>
     </IonPage>
   );
 };
