eaf190fab8b4c85a6d52ca66b6333f29172f11d0
hector <hectornuno1994@gmail.com>
2023-06-20 14:53:30 -0700

    sets state for users and loading using useState hook, creates function that makes fetch request to random users api, creates function to set users and loading state after fetch is finihsed, maps over users and dispays them in their own IonCards, adds button that removes all users, adds search bar

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,47 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React from "react";
+import {
+  IonAvatar,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonChip,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonPage,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+  useIonViewWillEnter,
+} from "@ionic/react";
+import { trashBinSharp } from "ionicons/icons";
+import React, { useState } from "react";
 
 const List: React.FC = () => {
+  const [users, setUsers] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useIonViewWillEnter(async () => {
+    const users = await getUsers();
+    console.log(users);
+    setUsers(users);
+    setLoading(false);
+  });
+
+  const getUsers = async () => {
+    const data = await fetch("https://randomuser.me/api?results=10");
+    const users = await data.json();
+    return users.results;
+  };
+
+  const clearList = () => {};
+
   return (
     <IonPage>
       <IonHeader>
@@ -9,10 +49,46 @@ const List: React.FC = () => {
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
+
           <IonTitle>List</IonTitle>
+
+          <IonButtons slot="end">
+            <IonButton>
+              <IonIcon slot="icon-only" icon={trashBinSharp} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+
+        <IonToolbar color={"primary"}>
+          <IonSearchbar />
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+
+      <IonContent>
+        {users.map((user, index) => (
+          <IonCard key={index}>
+            <IonCardHeader>
+              <IonCardSubtitle>{user.login.username}</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent className="ion-no-padding">
+              <IonItem lines="none">
+                <IonAvatar slot="start">
+                  <IonImg src={user.picture.thumbnail} />
+                </IonAvatar>
+                <IonLabel>
+                  {user.name.first} {user.name.last}
+                  <p> {user.email} </p>
+                </IonLabel>
+
+                <IonChip slot="end" color={"secondary"}>
+                  {user.location.country}
+                </IonChip>
+              </IonItem>
+            </IonCardContent>
+          </IonCard>
+        ))}
+      </IonContent>
     </IonPage>
   );
 };
